6d7f9c22be0c70c7f80cc8eec4ccdd3aa1f66cf3
Haris <haris.usman@hotmail.com>
2022-12-17 15:38:31 +0100

    Fixed routing behaviour

--- src/js/handlers/logout.mjs
+++ src/js/handlers/logout.mjs
@@ -4,6 +4,6 @@ export function handleLogButton() {
   const logoutButton = document.querySelector(".log-button");
   logoutButton.addEventListener("click", () => {
     logout();
-    location.href = "/sp-2/login.html";
+    location.href = "/login.html";
   });
 }
